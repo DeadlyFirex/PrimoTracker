8dc829ca558e661dfbee32070b6b637a94873871
Deadly <info.saddiki@gmail.com>
2023-12-14 23:32:56 +0100

    Rewrote factory, split components, added flask-marshmallow
    Note: This commit alone will not work properly.
    - Split initialization and declaration of components to seperate files
    - Database initialization check moved to database.py
    - Optimized imports
    - Minor changes to create_app / run to make config configurable

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,25 +1,19 @@
 from flask import Flask
-from flask_limiter import Limiter
-from flask_jwt_extended import JWTManager
-from flask_limiter.util import get_remote_address
-from sqlalchemy.exc import OperationalError
+from python_json_config import config_node
 
-from flaskr import admin, auth, generics, user
 from services.config import Config
-from services.database import initialize_database
-from services.utilities import generate_database_url, generate_storage_url
+from services.database import verify_database
+from services.utilities import generate_database_url
 
 from secrets import token_hex
 
-# Get configuration, create Flask application
-config = Config().get()
 
-
-def create_app():
+def create_app(config: config_node.Config = Config().get()) -> Flask:
     app = Flask(config.application.name)
 
     # Setup configuration
     app.config.from_mapping(
+        NAME=config.application.name,
         DEBUG=config.application.debug,
         SECRET_KEY=token_hex(),
         DATABASE=generate_database_url(),
@@ -27,16 +21,15 @@ def create_app():
         JWT_SECRET_KEY=token_hex(),
     )
 
-    # Configure blueprints/views and ratelimiting
-    # TODO: Make every limit configurable
-    limiter = Limiter(get_remote_address,
-                      app=app,
-                      enabled=config.ratelimit.enabled,
-                      default_limits=config.ratelimit.limits.default,
-                      storage_uri=generate_storage_url(),
-                      headers_enabled=config.ratelimit.headers
-                      )
+    # Register JWT / Limiter / Marshmallow
+    from services.authentication import jwt
+    jwt.init_app(app)
 
+    from services.limits import limiter
+    limiter.init_app(app)
+
+    # Configure blueprints/views and ratelimiting
+    from flaskr import admin, auth, generics, user
     for limit, endpoint in [(config.ratelimit.limits.admin, admin.admin),
                             (config.ratelimit.limits.authentication, auth.auth),
                             (config.ratelimit.limits.default, generics.generics),
@@ -45,34 +38,17 @@ def create_app():
             limiter.limit(counter)(endpoint)
         app.register_blueprint(endpoint)
 
-    # Register JWT
-    JWTManager(app).init_app(app)
+    from services.database import ma
+    ma.init_app(app)
 
     # Check database status
-    # TODO: Improve this function to be more flexible.
-    def first_time_run():
-        from models import user, index, audit
-        attempted = False
-        if config.database.type == "memory":
-            app.logger.warning("Warning: You are using a memory database. "
-                               "Most functions besides starting will not function properly."
-                               "Do NOT use this in production.")
-        app.logger.info("Checking for database initialization.")
-
-        try:
-            if any(not table.query.all() for table in [user.User, audit.AuditAction, index.UUIDIndex]):
-                initialize_database()
-                attempted = True
-                app.logger.info("Performing new database initialization or repopulating tables.")
-        except OperationalError:
-            if not attempted:
-                initialize_database()
-            app.logger.info("Performing new database initialization.")
-
-    first_time_run()
+    verify_database()
 
     return app
 
 
 if __name__ == "__main__":
-    create_app().run(config.server.host, config.server.port, config.server.debug)
+    from sys import argv
+    app_config = Config(path=argv[1] if len(argv) > 1 else None).get()
+
+    create_app().run(app_config.server.host, app_config.server.port, app_config.server.debug)
